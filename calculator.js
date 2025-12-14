15ea82cd08ef12b728bdbce4f17d9561e4dc8ee8
beckykhoo <beckykhoo04@gmail.com>
2025-12-14 05:50:02 +0100

    Created visual skeleton of calculator and basic calculation functions

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            throw new Error("Invalid operator.");
+    }
+}
